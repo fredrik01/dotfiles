b190bbdc70c52678a9840fab714c1ea25e399545
Fredrik Andersson <fredrikandersson01@gmail.com>
2022-08-02 15:29:04 +0200

    Disable line numbers in zen mode

--- nvim/after/plugin/zen-mode.nvim.lua
+++ nvim/after/plugin/zen-mode.nvim.lua
@@ -11,7 +11,7 @@ require("zen-mode").setup {
         -- uncomment any of the options below, or add other vim.wo options you want to apply
         options = {
           -- signcolumn = "no", -- disable signcolumn
-          number = true, -- disable number column
+          number = false, -- disable number column
           -- relativenumber = false, -- disable relative numbers
           -- cursorline = false, -- disable cursorline
           -- cursorcolumn = false, -- disable cursor column
