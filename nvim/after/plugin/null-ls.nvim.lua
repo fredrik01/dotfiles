0cbac072a873628c84561ff95aa0afc2b419bfca
Fredrik Andersson <fredrikandersson01@gmail.com>
2022-11-04 16:27:40 +0100

    Remove gitsigns code actions

--- nvim/after/plugin/null-ls.nvim.lua
+++ nvim/after/plugin/null-ls.nvim.lua
@@ -14,8 +14,6 @@ local sources = {
     null_ls.builtins.diagnostics.hadolint,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.vint,
-
-    null_ls.builtins.code_actions.gitsigns,
 }
 
 null_ls.setup({ sources = sources })
