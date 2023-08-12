a5e943d1e008ff2e1e0180a594b77be2a0695e02
Fredrik Andersson <fredrikandersson01@gmail.com>
2023-08-12 23:00:27 +0200

    Add possible config for "nvim diff against main" with fzf-lua

--- nvim/after/plugin/fzf-lua.lua
+++ nvim/after/plugin/fzf-lua.lua
@@ -1,4 +1,6 @@
 _G.fzf_diff_against_main = function()
+  -- This works in mono repos, like salary
+  -- require'fzf-lua'.fzf_exec("git diff --name-only $(git symbolic-ref refs/remotes/origin/HEAD | cut -d'/' -f4) --line-prefix=$(git rev-parse --show-toplevel)/", {
   require'fzf-lua'.fzf_exec("git diff --name-only $(git symbolic-ref refs/remotes/origin/HEAD | cut -d'/' -f4)", {
 		-- Diff against the main branch
     preview = "git diff $(git symbolic-ref refs/remotes/origin/HEAD | cut -d'/' -f4) {} | delta",
