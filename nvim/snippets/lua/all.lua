2868392964f764fc00cc6590eb421641c5d9322d
Fredrik Andersson <fredrikandersson01@gmail.com>
2023-11-09 20:43:24 +0100

    Add general snippets for yesterday and tomorrow

--- nvim/snippets/lua/all.lua
+++ nvim/snippets/lua/all.lua
@@ -2,16 +2,44 @@ local ls = require("luasnip")
 local snip = ls.snippet
 local func = ls.function_node
 
-local date = function() return {os.date('%Y-%m-%d')} end
+local today = function() return { os.date('%Y-%m-%d') } end
+
+local tomorrow = function()
+    local todayTime = os.time(os.date('*t'))
+    -- Add one day (86400 seconds)
+    local tomorowTime = todayTime + 86400
+    return os.date('%Y-%m-%d', tomorowTime)
+end
+
+local yesterday = function()
+    local todayTime = os.time(os.date('*t'))
+    -- Subtract one day (86400 seconds)
+    local yesterdayTime = todayTime - 86400
+    return os.date('%Y-%m-%d', yesterdayTime)
+end
 
 ls.add_snippets(nil, {
     all = {
         snip({
-            trig = "date",
-            namr = "Date",
+            trig = "today",
+            namr = "Today",
             dscr = "Date in the form of YYYY-MM-DD",
         }, {
-            func(date, {}),
+            func(today, {}),
+        }),
+        snip({
+            trig = "tomorrow",
+            namr = "Tomorrow",
+            dscr = "Date in the form of YYYY-MM-DD",
+        }, {
+            func(tomorrow, {}),
+        }),
+        snip({
+            trig = "yesterday",
+            namr = "Yesterday",
+            dscr = "Date in the form of YYYY-MM-DD",
+        }, {
+            func(yesterday, {}),
         }),
     },
 })
